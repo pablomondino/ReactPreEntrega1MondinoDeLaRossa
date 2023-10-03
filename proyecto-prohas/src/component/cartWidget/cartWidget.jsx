071d9d15a4a829a59bb487887988d44e46d75b11
Pablo Mondino De La Rossa <p_mondino@hotmail.com>
2023-10-03 07:06:48 -0300

    armando componentes

---
+++ proyecto-prohas/src/component/cartWidget/cartWidget.jsx
@@ -0,0 +1,9 @@
+const CartWitget = ()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default CartWitget
---
+++ proyecto-prohas/src/component/navBar/navBar.jsx
@@ -0,0 +1,9 @@
+const NavBar=()=>{
+    return(
+    <div>
+        <p>Prohas - Lotes con servicios</p>
+        aca va tu navBar
+    </div>
+    )
+}
+export default NavBar
